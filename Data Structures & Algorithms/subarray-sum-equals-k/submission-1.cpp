af97060d5134018921d2e680e3350d9b166379c0
AkashChaudhary180 <payalamit686@gmail.com>
2026-04-28 20:33:10 +0530

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        int ans = 0;
+
+        for(int i =0;i<nums.size();i++){
+            int cur = 0;
+            for(int j =i;j<nums.size();j++){
+
+                cur+=nums[j];
+                if(cur==k) ans++;
+
+            }
+        } 
+
+        return ans;
+    }
+};
