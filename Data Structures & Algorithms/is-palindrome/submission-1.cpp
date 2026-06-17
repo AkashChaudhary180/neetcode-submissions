bcd674169177fc29e37fa20bce90101446b9e958
Akash Chaudhary <payalamit686@gmail.com>
2026-06-17 11:57:34 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length()-1;
+
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        while(i<j){
+            
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
+            
+            if(s[i]!=s[j]) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
